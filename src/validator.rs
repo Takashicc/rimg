c84da944557f9c6f5b6edd8da1655f1f1d8ced84
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-10-24 20:12:37 +0900

    test: Add test for validator

--- src/validator.rs
+++ src/validator.rs
@@ -26,3 +26,77 @@ pub fn start_from_zero(s: &str) -> Result<u8, String> {
         .map_err(|_| format!("`{}` isn't a number or starts from zero", s))?;
     Ok(digit)
 }
+
+#[cfg(test)]
+mod tests {
+
+    mod dir_exists {
+        use super::super::*;
+        use std::path::PathBuf;
+
+        #[test]
+        fn existed_dir_should_return_path() {
+            let path = env!("CARGO_MANIFEST_DIR");
+            let result = dir_exists(path).unwrap();
+            assert_eq!(path, result);
+        }
+
+        #[test]
+        #[should_panic]
+        fn non_existed_dir_should_panic() {
+            let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+            path.push("dummy");
+            dir_exists(String::from(path.to_string_lossy()).as_str()).unwrap();
+        }
+    }
+
+    mod is_positive_number {
+        use super::super::*;
+
+        #[test]
+        #[should_panic]
+        fn minus_one_should_panic() {
+            is_positive_number("-1").unwrap();
+        }
+
+        #[test]
+        #[should_panic]
+        fn zero_should_panic() {
+            is_positive_number("0").unwrap();
+        }
+
+        #[test]
+        fn one_should_return_number() {
+            let result = is_positive_number("1").unwrap();
+            assert_eq!(1, result);
+        }
+
+        #[test]
+        #[should_panic]
+        fn string_should_panic() {
+            is_positive_number("hello").unwrap();
+        }
+    }
+
+    mod start_from_zero {
+        use super::super::*;
+
+        #[test]
+        #[should_panic]
+        fn minus_one_should_panic() {
+            start_from_zero("-1").unwrap();
+        }
+
+        #[test]
+        fn zero_should_return_number() {
+            let result = start_from_zero("0").unwrap();
+            assert_eq!(0, result);
+        }
+
+        #[test]
+        fn one_should_return_number() {
+            let result = start_from_zero("1").unwrap();
+            assert_eq!(1, result);
+        }
+    }
+}
