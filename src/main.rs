edf1fd9bcf0ff64d500ffad8e22cb3028b39f407
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-10-24 19:49:57 +0900

    feat: Get rename command input parameter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,80 @@
+mod validator;
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Rename files in each directory to sequential number
+    Rename {
+        #[arg(
+            value_parser = validator::dir_exists,
+            help = "Target directory"
+        )]
+        input_dir: String,
+
+        #[arg(
+            short,
+            long,
+            default_value_t = 4,
+            value_parser = validator::is_positive_number,
+            help = "Number of digits for renaming"
+        )]
+        digit: u8,
+        #[arg(
+            short,
+            long,
+            default_value_t = String::from("jpg"),
+            help = "Target file extension"
+        )]
+        extension: String,
+
+        #[arg(
+            short,
+            long,
+            default_value_t = 1,
+            value_parser = validator::start_from_zero,
+            help = "Initial number"
+        )]
+        initial: u8,
+
+        #[arg(
+            short,
+            long,
+            default_value_t = 1,
+            value_parser = validator::is_positive_number,
+            help = "Number of steps to count each files"
+        )]
+        step: u8,
+
+        #[arg(short, long, help = "Execute immediately or not")]
+        yes: bool,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    match &cli.command {
+        Some(Commands::Rename {
+            input_dir,
+            digit,
+            extension: extensions,
+            initial,
+            step,
+            yes,
+        }) => {
+            println!("input_dir, {}", input_dir);
+            println!("digit, {}", digit);
+            println!("extensions, {}", extensions);
+            println!("initial, {}", initial);
+            println!("step, {}", step);
+            println!("yes, {}", yes);
+        }
+        None => panic!("No sub command provided!"),
+    }
 }
