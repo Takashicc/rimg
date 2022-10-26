491057893646162ec4aa388f0e5639c36c70cb4b
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-10-26 19:07:33 +0900

    refactor: Move RenameParams to params file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod executor;
+mod params;
 mod validator;
 use clap::{Parser, Subcommand};
 
@@ -12,54 +13,7 @@ struct Cli {
 #[derive(Subcommand)]
 enum Commands {
     /// Rename files in each directory to sequential number
-    Rename(RenameParams),
-}
-
-#[derive(Parser)]
-pub struct RenameParams {
-    #[arg(
-        value_parser = validator::dir_exists,
-        help = "Target directory"
-    )]
-    input_dir: String,
-
-    #[arg(
-        short,
-        long,
-        default_value_t = 4,
-        value_parser = validator::is_positive_number,
-        help = "Number of digits for renaming"
-    )]
-    digit: u8,
-
-    #[arg(
-        short,
-        long,
-        default_value_t = String::from("jpg"),
-        help = "Target file extension"
-    )]
-    extension: String,
-
-    #[arg(
-        short,
-        long,
-        default_value_t = 1,
-        value_parser = validator::start_from_zero,
-        help = "Initial number"
-    )]
-    initial: u8,
-
-    #[arg(
-        short,
-        long,
-        default_value_t = 1,
-        value_parser = validator::is_positive_number,
-        help = "Number of steps to count each files"
-    )]
-    step: u8,
-
-    #[arg(short, long, help = "Execute immediately or not")]
-    yes: bool,
+    Rename(params::RenameParams),
 }
 
 fn main() {
