1c00e2a5cde76da24cc85255b778231fe7a0a80d
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-10-28 14:30:17 +0900

    feat: Validate all the compressed files when validate flag is true

--- src/executor/compress.rs
+++ src/executor/compress.rs
@@ -1,8 +1,10 @@
 use crate::constant::RAR_PATH;
 use crate::executor::utils::{ask, is_dir, is_hidden, is_parent};
 use crate::params::compress::CompressParams;
+use colored::Colorize;
 use execute::Execute;
 use indicatif::{ProgressBar, ProgressStyle};
+use std::collections::HashMap;
 use std::process::{self, Command, Stdio};
 use walkdir::{DirEntry, WalkDir};
 
@@ -43,6 +45,7 @@ pub fn execute(params: &CompressParams) {
             .progress_chars("##>-"),
     );
 
+    let mut compressed_files = HashMap::<String, bool>::new();
     for directory in directories {
         let filename = format!("{}.rar", directory.path().to_str().unwrap());
         bar.set_message(format!("Compressing {}", &filename));
@@ -68,6 +71,7 @@ pub fn execute(params: &CompressParams) {
 
         if let Some(exit_code) = command.execute().unwrap() {
             if exit_code == 0 {
+                compressed_files.insert(filename.clone(), false);
                 bar.set_message(format!("Compressed {}!", &filename));
             } else {
                 bar.set_message(format!("Failed to compress {}!", &filename));
@@ -78,8 +82,51 @@ pub fn execute(params: &CompressParams) {
 
         bar.inc(1);
     }
-
     bar.finish();
 
-    // TODO After compressing, validate the compressed files when validate flag is true
+    // Validate compressed files
+    if params.validate {
+        validate_files(&params.input_dir, compressed_files);
+    }
+}
+
+fn validate_files(input_dir: &str, mut files: HashMap<String, bool>) {
+    let bar = ProgressBar::new(files.len() as u64).with_style(
+        ProgressStyle::default_bar()
+            .template("|{bar:60.green/blue}| {pos:5}/{len:5} {msg}")
+            .unwrap()
+            .progress_chars("##>-"),
+    );
+    for (filename, compress_success) in files.iter_mut() {
+        let mut command = Command::new(RAR_PATH);
+        command.args(vec!["t", "--", filename.as_str()]);
+        command.current_dir(input_dir);
+        command.stdout(Stdio::null());
+
+        bar.set_message(format!("Validating {}", filename));
+
+        if let Some(exit_code) = command.execute().unwrap() {
+            if exit_code == 0 {
+                *compress_success = true;
+                bar.set_message("OK");
+            } else {
+                bar.set_message("NG");
+            }
+        } else {
+            bar.set_message("Interrupted!");
+        }
+
+        bar.inc(1);
+    }
+    bar.finish();
+
+    let invalid_files: HashMap<_, _> = files.iter().filter(|&(_, valid)| !(*valid)).collect();
+    if !invalid_files.is_empty() {
+        println!("{}", "The corrupted files are listed below".red().bold());
+        for &invalid_file in invalid_files.keys() {
+            println!("{}", invalid_file);
+        }
+    } else {
+        println!("{}", "All files can be unpacked".green().bold());
+    }
 }
