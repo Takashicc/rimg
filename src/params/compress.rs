2332ed48e0112e58a926de1ff0f337fd3de58998
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-10-28 13:47:43 +0900

    refactor: Split rename param and compress param to each file

---
+++ src/params/compress.rs
@@ -0,0 +1,46 @@
+use crate::constant::RAR_PATH;
+use crate::validator;
+use clap::Parser;
+
+#[derive(Parser)]
+pub struct CompressParams {
+    #[arg(
+        value_parser = validator::dir_exists,
+        help = "Input directory"
+    )]
+    pub input_dir: String,
+
+    #[arg(
+        short,
+        long,
+        value_parser = validator::dir_exists,
+        help = "Output directory"
+    )]
+    pub output_dir: Option<String>,
+
+    #[arg(
+        short,
+        long,
+        default_value_t = String::from(RAR_PATH),
+        value_parser = validator::format_type_check,
+        help = "Compress file format type"
+    )]
+    pub format_type: String,
+
+    #[arg(
+        long,
+        conflicts_with = "validate_only",
+        help = "Check the compressed file is not corrupted after the file was created"
+    )]
+    pub validate: bool,
+
+    #[arg(
+        long,
+        conflicts_with = "validate",
+        help = "Just check the compressed file is not corrupted"
+    )]
+    pub validate_only: bool,
+
+    #[arg(short, long, help = "Execute immediately or not")]
+    pub yes: bool,
+}
