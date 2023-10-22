645e5dee733a97da44357a345b16b0f2c90cc179
Takashicc <105186894+Takashicc@users.noreply.github.com>
2023-10-23 08:47:30 +0900

    refactor: Change compression/validation result display. Make zip validation parallelize

--- src/executor/compress.rs
+++ src/executor/compress.rs
@@ -59,7 +59,7 @@ pub fn execute(params: &CompressParams) {
 
         ask(params.yes);
 
-        validate_files(params, files);
+        validate_files(params, &files);
         process::exit(0);
     }
 
@@ -80,7 +80,7 @@ pub fn execute(params: &CompressParams) {
 
     let success_files = compress_files(params, &directories);
     if params.validate {
-        validate_files(params, success_files);
+        validate_files(params, &success_files);
     }
 }
 
@@ -111,27 +111,16 @@ fn compress_files(params: &CompressParams, directories: &Vec<DirEntry>) -> HashM
     // Show compression result
     let success_files_clone = success_files.lock().unwrap().clone();
     let error_files_clone = error_files.lock().unwrap().clone();
-    println!("{}", "Compression Result".green().bold());
-    println!("# ----------------- #");
+
     println!(
-        "| {} |",
-        format!("Total    ->  {: >4}", directories.len())
-            .blue()
-            .bold()
+        "Compression result: {}/{}/{} = {}/{}/{}",
+        "Total".blue().bold(),
+        "Success".green().bold(),
+        "Error".red().bold(),
+        format!("{}", execute_target_len).blue().bold(),
+        format!("{}", success_files_clone.len()).green().bold(),
+        format!("{}", error_files_clone.len()).red().bold(),
     );
-    println!(
-        "| {} |",
-        format!("Created  ->  {: >4}", success_files_clone.len())
-            .green()
-            .bold()
-    );
-    println!(
-        "| {} |",
-        format!("Error    ->  {: >4}", error_files_clone.len())
-            .red()
-            .bold()
-    );
-    println!("# ----------------- #");
 
     // Show compress error directories
     if !error_files_clone.is_empty() {
@@ -293,7 +282,7 @@ fn compress_zip(
 }
 
 /// Returns output filepath
-/// If the ouput_dir was specified, use output_dir for the output filepath
+/// If the output_dir was specified, use output_dir for the output filepath
 /// Otherwise, input_dir is used
 ///
 /// # Arguments
@@ -360,7 +349,7 @@ fn _get_path_entries(directory: &DirEntry) -> Vec<PathBuf> {
 ///
 /// * `params` - Compress params
 /// * `files` - Filepaths to validate
-fn validate_files(params: &CompressParams, mut files: HashMap<String, bool>) {
+fn validate_files(params: &CompressParams, files: &HashMap<String, bool>) {
     let output_dir = if let Some(v) = &params.output_dir {
         v
     } else {
@@ -369,41 +358,31 @@ fn validate_files(params: &CompressParams, mut files: HashMap<String, bool>) {
 
     let bar = get_progress_bar(files.len() as u64);
 
-    match params.format_type.as_str() {
-        RAR_EXTENSION => {
-            validate_rar(&mut files, output_dir, &bar);
-        }
-        ZIP_EXTENSION => {
-            validate_zip(&mut files, output_dir, &bar);
-        }
+    let validation_result = match params.format_type.as_str() {
+        RAR_EXTENSION => validate_rar(&files, output_dir, &bar),
+        ZIP_EXTENSION => validate_zip(&files, output_dir, &bar),
         _ => unimplemented!(),
-    }
+    };
 
     bar.finish();
 
-    let invalid_files: HashMap<_, _> = files.iter().filter(|&(_, valid)| !(*valid)).collect();
-    let valid_files_len = files.len() - invalid_files.len();
+    let invalid_files: HashMap<_, _> = validation_result
+        .iter()
+        .filter(|&(_, valid)| !(*valid))
+        .collect();
+    let valid_files_len = validation_result.len() - invalid_files.len();
+    let invalid_files_len = invalid_files.len();
 
     // Show validation result
-    println!("{}", "Validation Result".green().bold());
-    println!("# ----------------- #");
     println!(
-        "| {} |",
-        format!("Total    ->  {: >4}", files.len()).blue().bold()
+        "Validation result: {}/{}/{} = {}/{}/{}",
+        "Total".blue().bold(),
+        "Valid".green().bold(),
+        "Invalid".red().bold(),
+        format!("{}", validation_result.len()).blue().bold(),
+        format!("{}", valid_files_len).green().bold(),
+        format!("{}", invalid_files_len).red().bold()
     );
-    println!(
-        "| {} |",
-        format!("Valid    ->  {: >4}", valid_files_len)
-            .green()
-            .bold()
-    );
-    println!(
-        "| {} |",
-        format!("Invalid  ->  {: >4}", invalid_files.len())
-            .red()
-            .bold()
-    );
-    println!("# ----------------- #");
 
     if !invalid_files.is_empty() {
         println!("{}", "The corrupted files are listed below".red().bold());
@@ -420,30 +399,47 @@ fn validate_files(params: &CompressParams, mut files: HashMap<String, bool>) {
 /// * `files` - Filepaths to validate
 /// * `current_dir` - Current directory
 /// * `bar` - Progress bar
-fn validate_rar(files: &mut HashMap<String, bool>, current_dir: &str, bar: &ProgressBar) {
-    for (filename, compress_success) in files.iter_mut() {
+///
+/// # Returns
+///
+/// A new HashMap containing the validation result
+fn validate_rar(
+    files: &HashMap<String, bool>,
+    current_dir: &str,
+    bar: &ProgressBar,
+) -> HashMap<String, bool> {
+    let validated_files = Arc::new(Mutex::new(files.clone()));
+
+    files.par_iter().for_each(|(filename, _)| {
         let mut command = Command::new(RAR_EXTENSION);
         command.args(vec!["t", "--", filename.as_str()]);
         command.current_dir(current_dir);
 
         bar.set_message(format!("Validating {}", filename));
 
-        match command.execute() {
+        let is_valid = match command.execute() {
             Ok(Some(exit_code)) => {
                 if exit_code == 0 {
-                    *compress_success = true;
                     bar.set_message("OK");
+                    true
                 } else {
                     bar.set_message("NG");
+                    false
                 }
             }
             _ => {
                 bar.set_message("NG");
+                false
             }
-        }
+        };
 
+        let mut validated_files = validated_files.lock().unwrap();
+        validated_files.insert(filename.to_string(), is_valid);
         bar.inc(1);
-    }
+    });
+
+    let validated_files = validated_files.lock().unwrap().clone();
+    validated_files
 }
 
 /// Validate zip files
@@ -453,27 +449,64 @@ fn validate_rar(files: &mut HashMap<String, bool>, current_dir: &str, bar: &Prog
 /// * `files` - Filepaths to validate
 /// * `current_dir` - Current directory
 /// * `bar` - Progress bar
-fn validate_zip(files: &mut HashMap<String, bool>, current_dir: &str, bar: &ProgressBar) {
-    for (filename, compress_success) in files.iter_mut() {
+///
+/// # Returns
+///
+/// A new HashMap containing the validation result
+fn validate_zip(
+    files: &HashMap<String, bool>,
+    current_dir: &str,
+    bar: &ProgressBar,
+) -> HashMap<String, bool> {
+    let validated_files = Arc::new(Mutex::new(files.clone()));
+
+    files.par_iter().for_each(|(filename, _)| {
         let fullpath = Path::new(current_dir).join(filename);
         bar.set_message(format!("Validating {}", filename));
 
-        let file = match File::open(fullpath) {
+        let is_valid = match validate_zip_entries(&fullpath) {
             Ok(v) => v,
-            Err(_) => {
-                bar.set_message("NG");
-                bar.inc(1);
-                continue;
+            Err(e) => {
+                eprintln!("filename: {}, error: {}", filename, e);
+                false
             }
         };
-        let zip_archive = ZipArchive::new(file);
-        if zip_archive.is_ok() {
-            *compress_success = true;
-            bar.set_message("OK");
-        } else {
-            bar.set_message("NG");
-        }
 
+        let mut validated_files = validated_files.lock().unwrap();
+        validated_files.insert(filename.to_string(), is_valid);
         bar.inc(1);
+    });
+
+    let validated_files = validated_files.lock().unwrap().clone();
+    validated_files
+}
+
+fn validate_zip_entries(filepath: &PathBuf) -> Result<bool, String> {
+    let file = match File::open(filepath) {
+        Ok(v) => v,
+        Err(e) => return Err(e.to_string()),
+    };
+
+    let mut archive = match ZipArchive::new(file) {
+        Ok(v) => v,
+        Err(e) => return Err(e.to_string()),
+    };
+
+    let mut buffer = vec![0; 1024];
+    for i in 0..archive.len() {
+        let mut file = match archive.by_index(i) {
+            Ok(v) => v,
+            Err(e) => return Err(e.to_string()),
+        };
+
+        loop {
+            match file.read(&mut buffer) {
+                Ok(0) => break,
+                Ok(_) => continue,
+                Err(e) => return Err(e.to_string()),
+            }
+        }
     }
+
+    Ok(true)
 }
